4ac87f0a9a1463172a3da4869c86d7f681cb1197
Noah Spitzer-Williams <noah@spitzer-williams.com>
2019-10-27 20:30:02 -0700

    reading google photo taken data. organized code

--- face-aging-slideshow.py
+++ face-aging-slideshow.py
@@ -3,18 +3,32 @@ import face_recognition
 import cv2
 import numpy as np
 import pathlib
+import json
 
-size = (640, 480)
-vvw = cv2.VideoWriter('slideshow.avi', cv2.VideoWriter_fourcc('X','V','I','D'), 24, size)
 
-py = pathlib.Path().glob("photos/*.jpg")
-for file in py:
-    print(str(file))
-    frame = cv2.imread("./" + str(file))
-    resized_frame = cv2.resize(frame, size, interpolation = cv2.INTER_CUBIC)
-    for x in range(1, 15):
-        vvw.write(resized_frame)
-vvw.release()
+def build_image_list():
+    image_dict = {}
+    py = pathlib.Path().glob(photos_path + "/*.jpg")
+    for file in py:
+        date_taken = get_photo_date_taken(str(file))
+        if date_taken:
+            image_dict[date_taken] = str(file)
+    
+    image_list = []
+    for elem in sorted(image_dict.keys()):
+        image_list.append(image_dict[elem])
+    return image_list
+
+
+def get_photo_date_taken(path):
+    json_path = path + ".json"
+    if os.path.exists(json_path) == False:
+        return None
+
+    with open(json_path, 'r') as f:
+        dict = json.load(f)
+        date_taken = dict["photoTakenTime"]["timestamp"]
+    return date_taken
 
 
 def get_face_image(path):
@@ -93,3 +107,25 @@ def get_face_image(path):
     image = cv2.resize(image, (300, 400), interpolation = cv2.INTER_CUBIC)
 
     return image
+
+
+def write_movie(file_paths):
+    vvw = cv2.VideoWriter('slideshow.mp4', cv2.VideoWriter_fourcc(*'avc1'), 30, size)
+
+    for file_path in file_paths:
+        print(file_path)
+        frame = cv2.imread("./" + file_path)
+        resized_frame = cv2.resize(frame, size, interpolation = cv2.INTER_CUBIC)
+        for x in range(1, 15):
+            vvw.write(resized_frame)
+    vvw.release()
+
+
+
+size = (1280, 720)
+days_per_min = 365
+photos_path = "photos"
+
+image_list = build_image_list()
+write_movie(image_list)
+
